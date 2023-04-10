20f993fe2f094f2a316190cdbe0d85e75fd6f240
himelhasan <himelhasan497@gmail.com>
2023-04-10 23:50:15 +0600

    server updated with new mongodb connection, created new endPoint - single category api, batchwise data, categorywise  data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 8000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const { query } = require("express");
 require("dotenv").config();
@@ -11,7 +11,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qnzupqp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8itgidz.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,13 +21,34 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const galleriesCollection = client.db("Alumni-project-data").collection("galleries");
-    const galleryCollection = client.db("Alumni-project-data").collection("gallery");
+    const galleriesCategory = client
+      .db("alumni-management-app")
+      .collection("alumniGalleryCategories");
+    const AllGalleryPhotos = client
+      .db("alumni-management-app")
+      .collection("allAlumniGalleryData");
 
-    //  gallery Category data
+    // api end points
+    // all gallery Category data
     app.get("/galleryCategories", async (req, res) => {
       const query = {};
-      const cursor = galleriesCollection.find(query);
+      const cursor = galleriesCategory.find(query);
+      const galleries = await cursor.toArray();
+      res.send(galleries);
+    });
+    // single gallery Category data
+    app.get("/galleryCategories/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const category = await galleriesCategory.findOne(query);
+      res.send(category);
+    });
+
+    // batch wise gallery Category data
+    app.get("/batchGallery/:batchNumber", async (req, res) => {
+      const batchNumber = req.params.batchNumber;
+      const query = { batchNumber: batchNumber };
+      const cursor = AllGalleryPhotos.find(query);
       const galleries = await cursor.toArray();
       res.send(galleries);
     });
@@ -34,7 +56,7 @@ async function run() {
     // all gallery data
     app.get("/galleries", async (req, res) => {
       const query = {};
-      const cursor = galleryCollection.find(query).limit(6);
+      const cursor = AllGalleryPhotos.find(query);
       const gallery = await cursor.toArray();
       res.send(gallery);
     });
@@ -42,8 +64,8 @@ async function run() {
     // CategoryWise gallery data
     app.get("/galleries/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { gellary_id: id };
-      const cursor = galleryCollection.find(query);
+      const query = { gallery_category_id: id };
+      const cursor = AllGalleryPhotos.find(query);
       const gallery = await cursor.toArray();
       res.send(gallery);
     });
